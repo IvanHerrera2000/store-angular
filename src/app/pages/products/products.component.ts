e58e6f68c8293c998166915834952dcadab272a0
unknown <ivanalejandroherrera2000@gmail.com>
2022-05-09 12:19:24 -0300

    getProducts method added

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { ProductsService } from './services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
+
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductsService) { }
 
   ngOnInit(): void {
+    this.productService.getProducts()
+    .pipe(
+      tap(res => console.log(res))
+    )
+    .subscribe()
   }
 
 }
